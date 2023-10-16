1c89b313673a3d79b3b438049c2d6c222c241c85
WillL <143326106+willl22@users.noreply.github.com>
2023-10-16 20:03:29 +0100

    add responsive design

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //show and hide desktop site image
 
 function showDSite() {
-    document.getElementById('swim-desktop').style.visibility = "visible";
-    document.getElementById('show-desktop').style.visibility = "hidden";
+    document.getElementById('swim-desktop').style.display = "block";
+    document.getElementById('show-desktop').style.display = "none";
 };
 
 document.getElementById('show-desktop').onclick = showDSite;
 
 function hideDSite() {
-    document.getElementById('show-desktop').style.visibility = "visible";
-    document.getElementById('swim-desktop').style.visibility = "hidden";
+    document.getElementById('show-desktop').style.display = "block";
+    document.getElementById('swim-desktop').style.display = "none";
 };
 
 document.getElementById('swim-desktop').onclick = hideDSite;
@@ -19,15 +19,15 @@ document.getElementById('swim-desktop').onclick = hideDSite;
 //show and hide tablet site image
 
 function showTSite() {
-    document.getElementById('swim-tablet').style.visibility = "visible";
-    document.getElementById('show-tablet').style.visibility = "hidden";
+    document.getElementById('swim-tablet').style.display = "block";
+    document.getElementById('show-tablet').style.display = "none";
 };
 
 document.getElementById('show-tablet').onclick = showTSite;
 
 function hideTSite() {
-    document.getElementById('show-tablet').style.visibility = "visible";
-    document.getElementById('swim-tablet').style.visibility = "hidden";
+    document.getElementById('show-tablet').style.display = "block";
+    document.getElementById('swim-tablet').style.display = "none";
 };
 
 document.getElementById('swim-tablet').onclick = hideTSite;
@@ -36,15 +36,15 @@ document.getElementById('swim-tablet').onclick = hideTSite;
 //show and hide mobile site image
 
 function showMSite() {
-    document.getElementById('swim-mobile').style.visibility = "visible";
-    document.getElementById('show-mobile').style.visibility = "hidden";
+    document.getElementById('swim-mobile').style.display = "block";
+    document.getElementById('show-mobile').style.display = "none";
 };
 
 document.getElementById('show-mobile').onclick = showMSite;
 
 function hideMSite() {
-    document.getElementById('show-mobile').style.visibility = "visible";
-    document.getElementById('swim-mobile').style.visibility = "hidden";
+    document.getElementById('show-mobile').style.display = "block";
+    document.getElementById('swim-mobile').style.display = "none";
 };
 
 document.getElementById('swim-mobile').onclick = hideMSite;
